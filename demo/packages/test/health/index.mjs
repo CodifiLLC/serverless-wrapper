f7cd4edfe39cf5427fdb5005252b02501a463759
Matthew Dickinson <matt@codifi.us>
2024-01-20 00:10:12 -0500

    add invokeFunction handling for express

--- demo/packages/test/health/index.mjs
+++ demo/packages/test/health/index.mjs
@@ -2,6 +2,7 @@ import {ok} from '@serverless-wrapper/core';
 import {randomStr} from "serverless-wrapper-lib/test.mjs";
 
 export async function main(wrapper) {
+    console.log('called health', wrapper.params);
     return ok({
         params: wrapper.params,
         randomStr: randomStr()
--- demo/packages/test/hello/index.mjs
+++ demo/packages/test/hello/index.mjs
@@ -3,5 +3,6 @@ import {randomStr} from "serverless-wrapper-lib/test.mjs";
 
 export function main(wrapper) {
     console.log('has been run', wrapper.getRemainingRuntime());
+    wrapper.invokeFunction('/test/health', {});
     return ok(`Hello, ${wrapper.getParams().name ?? 'world'}! Your val is ${randomStr()}`);
 }
